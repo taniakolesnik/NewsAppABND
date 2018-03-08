ed7112fbb25a07ef23b7cbeed538f55ce55da517
Tania Kolesnik <kolesnik@gmail.com>
2018-03-08 23:25:27 +0000

    Fixed onClick

--- app/src/main/java/com/example/android/newsappabnd/NewsRecyclerViewAdapter.java
+++ app/src/main/java/com/example/android/newsappabnd/NewsRecyclerViewAdapter.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.net.URL;
+import java.util.Collections;
 import java.util.List;
 
 import butterknife.BindView;
@@ -22,17 +23,18 @@ import butterknife.ButterKnife;
 
 public class NewsRecyclerViewAdapter extends RecyclerView.Adapter<NewsRecyclerViewAdapter.ViewHolder> {
 
+    Context context;
     private LayoutInflater layoutInflater;
-    private List<NewsStory> mData;
-    private String webLink;
+    private List<NewsStory> data = Collections.emptyList();
 
     public NewsRecyclerViewAdapter(Context context, List<NewsStory> data) {
+        this.context = context;
         this.layoutInflater = LayoutInflater.from(context);
-        this.mData = data;
+        this.data = data;
     }
 
     public void updateAdapter(List<NewsStory> mData) {
-        this.mData = mData;
+        this.data = mData;
         notifyDataSetChanged();
     }
 
@@ -45,7 +47,7 @@ public class NewsRecyclerViewAdapter extends RecyclerView.Adapter<NewsRecyclerVi
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View view = layoutInflater.inflate(R.layout.row_layout, parent, false);
-        return new ViewHolder(view, webLink);
+        return new ViewHolder(view);
     }
 
     /**
@@ -54,44 +56,42 @@ public class NewsRecyclerViewAdapter extends RecyclerView.Adapter<NewsRecyclerVi
      */
     @Override
     public void onBindViewHolder(NewsRecyclerViewAdapter.ViewHolder holder, int position) {
-        NewsStory item = mData.get(position);
+        NewsStory item = data.get(position);
         holder.titleTextView.setText(item.getNewsStory());
         holder.sectionTextView.setText(item.getSectionName());
-        holder.dateTextView.setText(item.getWebUrl());
-        if (!item.getAuthor().isEmpty()){
+        holder.dateTextView.setText(item.getPublicationDate());
+        if (!item.getAuthor().isEmpty()) {
             holder.authorTextView.setText(item.getAuthor());
             holder.authorTextView.setVisibility(View.VISIBLE);
         }
-        webLink = item.getWebUrl();
-
     }
-
     /**
      * This method returns the size of the collection that contains the items we want to display
+     *
      * @return number of items to be listed
      */
     @Override
     public int getItemCount() {
-        return mData.size();
+        return data.size();
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
-        public String webLink;
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         @BindView(R.id.title_textView) TextView titleTextView;
         @BindView(R.id.section_textView) TextView sectionTextView;
         @BindView(R.id.date_textView) TextView dateTextView;
         @BindView(R.id.author_textView) TextView authorTextView;
 
-        public ViewHolder(View itemView, String webLink) {
+        public ViewHolder(View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             itemView.setOnClickListener(this);
-            this.webLink = webLink;
         }
 
         @Override
         public void onClick(View view) {
-            Toast.makeText(view.getContext(), webLink + " position = " + getAdapterPosition(), Toast.LENGTH_SHORT).show();
+            NewsStory item = data.get(getAdapterPosition());
+            Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(item.getWebUrl()));
+            context.startActivity(intent);
         }
     }
 }
